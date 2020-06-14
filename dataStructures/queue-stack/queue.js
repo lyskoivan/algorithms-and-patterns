daaf5d0368d83ca2cfd428fdf6946a6499e4d026
Ivan <lysko1999i@gmail.com>
2020-06-14 22:18:05 +0300

    Queue

---
+++ dataStructures/queue-stack/queue.js
@@ -0,0 +1,12 @@
+var queue = []; // []
+queue.push("first"); // queue === ["first"]
+queue.push(10, 20); // queue === ["first", 10, 20]
+var el = queue.shift(); // queue === [10, 20] && el === "first"
+queue.push(2); // queue === [10, 20, 2]
+el = queue.shift(); // queue === [20, 2] && el === 10
+el = queue.shift(); // queue === [2] && el === 20
+el = queue.shift(); // queue === [] && el === 2
+el = queue.shift(); // queue === [] && typeof el === "undefined"
+
+// or with unshift and pop
+//  точно так же, только последний элемент окажется в начале, а первый в конце
